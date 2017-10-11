0b6210f52ab0502d1c72b72f4aa8f810010d5f78
RaNxxx <ranlanli.0821@gmail.com>
2017-10-11 23:04:18 +0900

    Q2とQ3を追加しました

--- checkio01.py
+++ checkio01.py
@@ -1,4 +1,5 @@
-#Q1: https://py.checkio.org/mission/most-wanted-letter/
+#Q1: The Most Wanted Letter
+#URL : https://py.checkio.org/mission/most-wanted-letter/
 
 import re
 
@@ -39,26 +40,68 @@ print(count_alphabet(text))
 
 def unique_elements(original_list):
 
-    #original_listからユニークの要素をnew_listに抽出
-    new_list = []
-    new_list.append(original_list[0])
+    #original_listの中の各要素の出現回数を算出
+    for x in original_list:
+        element_freq = { key : original_list.count(key) for key in original_list }
 
+    #element_freqの中で、value = 1のkeyのみ抽出し、new_listに入れる
+    new_list = []
+
+    for key,value in element_freq.items():
+        if value == 1:
+            new_list.append(key)
+
+    #original_listのelementが、new_listに入っていない場合は、result_listに入れる
+    result_list = []
 
     for element in original_list:
-        if element in original_list:
-            new_list.append(element)
+        if element not in new_list:
+            result_list.append(element)
 
-    #
+    return result_list
 
-    return original_list[0]
-
-original_list = [1, 2, 3, 1, 3]
+original_list = [10, 9, 10, 10, 9, 8]
 print(unique_elements(original_list))
 
 
 #Q3 : House Password
 #URL : https://py.checkio.org/mission/house-password/
 
-'''
-def check_pw(text):
-'''
+def check_password(password):
+
+    #passwordのtextを、listに変化
+    pw_list = [ element for element in password ]
+
+    #数字、大文字、小文字の出現したかどうかをチェック
+    check_list = []
+
+    for element in pw_list:
+        if element.islower() == True :
+            check_list.append("小文字")
+        elif element.isupper() == True :
+            check_list.append("大文字")
+        elif element.isdigit() == True :
+            check_list.append("数字")
+
+    #数字、大文字、小文字の出現回数の計算
+    check_freq = { key : check_list.count(key) for key in check_list }
+
+    #いずれも出現した場合はTrue、その他の場合はFalseを返す
+    true_dict = { "大文字" : 1, "小文字" : 1, "数字" : 1 }
+    result_list = []
+
+    for key,value in true_dict.items():
+        if key not in check_freq or len(check_list) < 10:
+            result_list.append(False)
+        else:
+            result_list.append(True)
+
+    for element in result_list:
+        if False not in result_list:
+            return True
+        else:
+            return False
+
+
+password = "QwErTy911poqqqq"
+print(check_password(password))
